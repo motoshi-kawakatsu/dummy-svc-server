e41cfba3e98e873242ef622fa34a1faf0fcb27cb
Motoshi Kawakatsu <kawaka2@gmail.com>
2018-11-26 12:44:59 +0900

    @RequestBody -> @RequestParam

--- src/main/java/server/service/dummy/controller/DummyMailgun.java
+++ src/main/java/server/service/dummy/controller/DummyMailgun.java
@@ -2,15 +2,15 @@ package server.service.dummy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import server.service.dummy.util.MailgunResponse;
 
-@RestController
+@Controller
 public class DummyMailgun {
 
 	@Autowired
@@ -20,7 +20,13 @@ public class DummyMailgun {
 
 	@RequestMapping(path="/messages", method=RequestMethod.POST)
 	@ResponseBody
-	public MailgunResponse messages(@RequestParam("to") String to) throws Exception {
+	public MailgunResponse messages(@RequestParam("to") String to,
+									 @RequestParam("cc") String cc,
+									 @RequestParam("bcc") String bcc,
+									 @RequestParam("from") String from,
+									 @RequestParam("h:Reply-To") String replyTo,
+									 @RequestParam("subject") String subject,
+									 @RequestParam("text") String text) throws Exception {
 		Thread.sleep(sleepTime);
 		return response;
 	}
