828d42b97751952b234c206b21371781281ca084
Motoshi Kawakatsu <kawaka2@gmail.com>
2018-11-26 12:38:22 +0900

    test

--- src/main/java/server/service/dummy/controller/DummyMailgun.java
+++ src/main/java/server/service/dummy/controller/DummyMailgun.java
@@ -2,13 +2,12 @@ package server.service.dummy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import server.service.dummy.util.MailgunRequestParam;
 import server.service.dummy.util.MailgunResponse;
 
 @RestController
@@ -21,7 +20,7 @@ public class DummyMailgun {
 
 	@RequestMapping(path="/messages", method=RequestMethod.POST)
 	@ResponseBody
-	public MailgunResponse messages(@RequestBody MailgunRequestParam param) throws Exception {
+	public MailgunResponse messages(@RequestParam("to") String to) throws Exception {
 		Thread.sleep(sleepTime);
 		return response;
 	}
